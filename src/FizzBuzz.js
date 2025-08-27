0991b5b6721ff12971ac8d7ebaf2258f9840c77c
Andres Zubieta <andres.zubieta@ucb.edu.bo>
2025-08-27 18:32:42 -0400

    feat: devuelve Fizz con cualquier numero multiplo de 3

--- src/FizzBuzz.js
+++ src/FizzBuzz.js
@@ -1,4 +1,10 @@
 function convertirFizzBuzz(x){
-    return ''+x;
+    if(x%3===0){
+        return 'Fizz';
+    }
+    else{
+        return ''+x;
+    }
+    
 }
 export {convertirFizzBuzz};
--- src/FizzBuzz.test.js
+++ src/FizzBuzz.test.js
@@ -8,4 +8,12 @@ import { convertirFizzBuzz } from "./FizzBuzz";
   it("debe devolver un numero cualquiera no multiplo de 3 o 5 como cadena", () => {
     expect(convertirFizzBuzz(7)).toEqual('7');
   });
+  
+  it("debe devolver Fizz a un numero multiplo de 3", () => {
+    expect(convertirFizzBuzz(6)).toEqual('Fizz');
+  });
+
+  it("debe devolver Fizz a cualquier numero multiplo de 3", () => {
+    expect(convertirFizzBuzz(9)).toEqual('Fizz');
+  });
 });
